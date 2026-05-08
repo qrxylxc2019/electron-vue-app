75468d74a26844956f42b5f4c9ae525945bd6871
lxc619 <lxc619@noreply.gitcode.com>
2026-05-08 20:46:35 +0800

    优化

--- src/renderer/src/views/Quiz.vue
+++ src/renderer/src/views/Quiz.vue
@@ -430,9 +430,8 @@ onMounted(() => {
   width: 120px;
   flex-shrink: 0;
   display: flex;
-  align-items: flex-start;
+  align-items: center;
   justify-content: center;
-  padding-top: 20px;
 }
 
 .progress-text {
